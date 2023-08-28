15d253a03718a2766203dabdb96ff26cf755f082
Dirk Koritnik <koritnik@werkraum-media.de>
2023-08-28 16:42:37 +0200

    Remove TYPO3_MODE constant

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,8 @@
 <?php
 
 declare(strict_types=1);
-defined('TYPO3_MODE') || die('Access denied.');
+
+defined('TYPO3') || die('Access denied from extension maps in localconf.');
 
 (function ($extKey='maps') {
 
